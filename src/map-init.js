b003ff00047a3aa23ae79a3c5e54266f3d484700
Joera <joeramulders@gmail.com>
2018-03-21 13:20:27 +0100

    knoppen

--- src/map-init.js
+++ src/map-init.js
@@ -622,9 +622,9 @@ class Map {
         self = this;
 
         if(self.session.route === 'old') {
-            self._showNew(routeId);
-        } else {
             self._showOld(routeId);
+        } else {
+            self._showNew(routeId);
         }
     }
 
