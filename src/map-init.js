eb25d37ae2107e49ca558aed33e4362dc31315cd
Joera <joeramulders@gmail.com>
2018-03-21 13:03:08 +0100

    knoppen

--- src/map-init.js
+++ src/map-init.js
@@ -25,7 +25,7 @@ class Map {
         }
         
         this.session = {
-            route : 'old';
+            route : 'old',
             origin : null,
             destination : null,
             data : {}
